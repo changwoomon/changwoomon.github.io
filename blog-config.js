7abaf3f156b16597198bec7797adc7deb078181c
changwoomon <kamme10@naver.com>
2021-05-17 02:19:08 +0900

    initial commit

---
+++ blog-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "changwoomon's blog",
+  description: "(੭•̀ᴗ•̀)੭🔥",
+  author: "changwoomon",
+  siteUrl: "https://changwoomon.github.io/",
+  links: {
+    github: "https://github.com/devHudi",
+    kaggle: "https://www.kaggle.com/changwoomon",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com/profile.php?id=100057724153835",
+    // instagram: "https://www.instagram.com/dawn_fromeast/",
+    // etc: "https://www.notion.so/Hudi-s-R-sum-0c1d1a1b35284d1eaf05c5bfac4a3cad",
+  },
+  utterances: {
+    repo: "changwoomon/changwoomon.github.io",
+    type: "pathname",
+  },
+}
