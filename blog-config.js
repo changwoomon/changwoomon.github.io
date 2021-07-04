9111f1b641fc261fc148ba688899dd90b8cc4efa
changwoomon <kamme10@naver.com>
2021-07-04 21:42:17 +0900

    update

--- blog-config.js
+++ blog-config.js
@@ -6,6 +6,7 @@ module.exports = {
   links: {
     github: "https://github.com/changwoomon",
     kaggle: "https://www.kaggle.com/changwoomon",
+    etc: "https://drive.google.com/file/d/1c4CHV_f_86UUUox_7sBYqPSSno2CebeE/view?usp=sharing",
     // linkedIn: "https://linkedin.com",
     // facebook: "https://www.facebook.com/profile.php?id=100057724153835",
     // instagram: "https://www.instagram.com/dawn_fromeast/",
