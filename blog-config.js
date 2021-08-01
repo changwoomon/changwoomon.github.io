927bab7b54bd75c29acbc8402314d4d8ef427d36
changwoomon <kamme10@naver.com>
2021-08-01 18:57:52 +0900

    update

--- blog-config.js
+++ blog-config.js
@@ -6,7 +6,7 @@ module.exports = {
   links: {
     github: "https://github.com/changwoomon",
     kaggle: "https://www.kaggle.com/changwoomon",
-    etc: "https://drive.google.com/file/d/1c4CHV_f_86UUUox_7sBYqPSSno2CebeE/view?usp=sharing",
+    etc: "https://drive.google.com/file/d/1LSwntP-sh4GePQ5FF9F6rq5TQLQg1Z-S/view?usp=sharing",
     // linkedIn: "https://linkedin.com",
     // facebook: "https://www.facebook.com/profile.php?id=100057724153835",
     // instagram: "https://www.instagram.com/dawn_fromeast/",
