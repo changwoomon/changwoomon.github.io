472e8c7c5771852f3845690129614c29a49ed0b5
changwoomon <kamme10@naver.com>
2021-11-17 23:38:32 +0900

    update

--- src/reducers/createStore.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux"
-import rootReducer from "reducers"
-
-export default preloadedState => {
-  return createStore(rootReducer, preloadedState)
-}
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import theme from "./theme"
-
-const rootReducer = combineReducers({
-  theme,
-})
-
-export default rootReducer
